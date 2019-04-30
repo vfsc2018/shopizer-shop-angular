627654c2ecc331bb0a948fd78ddad45020f346be
Stephen B <sushicandypizza@gmail.com>
2019-04-29 20:28:00 -0400

    Ajoute site header
    Display none sur le input recherche temporaire

---
+++ src/app/siteheader/siteheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'siteheader',
+  templateUrl: './siteheader.component.html',
+  styleUrls: ['./siteheader.component.sass']
+})
+export class SiteheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
