37520b4e6dc64c024fca7c9893ef5a0dedbe8d3d
Stephen B <sushicandypizza@gmail.com>
2019-05-02 20:59:43 -0400

    MAke animation of submenu the same as the cart

--- src/app/siteheader/siteheader.component.ts
+++ src/app/siteheader/siteheader.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'siteheader',
   templateUrl: './siteheader.component.html',
-  styleUrls: ['./siteheader.component.sass']
+  styleUrls: ['./siteheader.component.scss']
 })
 export class SiteheaderComponent implements OnInit {
 
