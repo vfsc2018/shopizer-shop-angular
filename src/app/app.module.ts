dc18f55831c46f6496135558e1f9d514a6322bd4
shopizer-ecommerce <csamson777@yahoo.com>
2019-05-06 20:50:32 -0400

    Execution of a service during startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, APP_INITIALIZER } from "@angular/core";
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { ConfigurationService } from './configuration.service';
 import { SiteheaderComponent } from './siteheader/siteheader.component';
 import { SearchComponent } from './search/search.component';
 import { CartComponent } from './cart/cart.component';
 
+export function loadConfigurations(configurationService: ConfigurationService) {
+  return () => configurationService.getConfigs();
+}
+
+/**
+ * Outch not easy to create a class executed at startup. I expect to be a better Angular dev,
+ * could barely undestand what i have done ... carl
+ */
+
 @NgModule({
   declarations: [AppComponent, SiteheaderComponent, SearchComponent, CartComponent],
-  imports: [NgbModule, BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [NgbModule, BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    ConfigurationService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadConfigurations,
+      deps: [ConfigurationService],
+      multi: true
+    }
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
