efbbf0c8492b8947484d03fdedf707a3c485e888
Nimble Sencha <jaimin@nimblechapps.com>
2019-09-12 16:33:54 +0530

    1) Changed in register page same as demo
    2) Check out page added shipping address box

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -29,6 +29,20 @@ export class CheckoutComponent implements OnInit {
     email: '',
     note: ''
   }
+  shipping = {
+    firstname: '',
+    lastname: '',
+    companyName: '',
+    address: '',
+    address1: '',
+    city: '',
+    country: '',
+    state: '',
+    postcode: '',
+    phone: '',
+    email: '',
+    note: ''
+  }
   stateData: Array<any> = [];
   countryData: Array<any> = [];
   // countyData: Array<any> = ['Belarus', 'Canada', 'Romania', 'United State'];
@@ -40,6 +54,41 @@ export class CheckoutComponent implements OnInit {
   ) {
     this.getCountry();
 
+  }
+  onSameBillingAddress(event) {
+    console.log(event.target.checked)
+    if (event.target.checked) {
+      this.shipping = {
+        firstname: this.checkout.firstname,
+        lastname: this.checkout.lastname,
+        companyName: this.checkout.companyName,
+        address: this.checkout.address,
+        address1: this.checkout.address1,
+        city: this.checkout.city,
+        state: this.checkout.state,
+        country: this.checkout.country,
+        postcode: this.checkout.postcode,
+        phone: this.checkout.phone,
+        email: this.checkout.email,
+        note: this.checkout.note,
+
+      }
+    } else {
+      this.shipping = {
+        firstname: '',
+        lastname: '',
+        companyName: '',
+        address: '',
+        address1: '',
+        city: '',
+        state: '',
+        country: '',
+        postcode: '',
+        phone: '',
+        email: '',
+        note: ''
+      }
+    }
   }
   getCountry() {
     let action = Action.COUNTRY;
