f74cd2d66f65a6563c7f35581eb62a940873c843
uconvert <uconvert@yahoo.com>
2020-10-12 19:38:42 +0700

    fix reviews

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'http://99.79.1.102:8080',
+  baseUrl: 'http://14.160.32.79:8081',
   apiVersion: '/api/v1/',
   merchant: 'DEFAULT'
 };
